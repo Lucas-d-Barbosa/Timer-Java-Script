c01dcdc122efe25dfe01ecb156b8595ac54d37ae
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-02-24 14:44:00 -0300

    Alterando as variáveis horas, minutos e segundos para elas iniciarem com 0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,12 @@
-// (function(){
-//     const horas = 0;
-//     const minutos = 0;
-//     const segundos = 0;
-//     const horario = {
-//         segundos: segundos,
-//         minutos: minutos,
-//         horas: horas
-//     }
-//     const timer = document.querySelector(".timer");
-//     timer.innerHTML = `0${horas}:0${minutos}:0${segundos}`;
-
-//     const iniciar = document.querySelector(".iniciar");
-//     const pausar = document.querySelector(".pausar");
-//     const zerar = document.querySelector(".zerar");
-//     let intervalID;
-//     iniciar.addEventListener("click", () =>{
-//         intervalID = setInterval(mudarHora, 1000, horario, timer);
-//     });
-
-//     function mudarHora(horario, timer){
-//         horario.segundos = horario.segundos + 1;
-//         if(horario.segundos === 60){
-//             horario.segundos = 0;
-//             horario.minutos = horario.minutos + 1;
-//         }
-//         if(horario.minutos === 60)
-//             {
-//                 horario.minutos = 0;
-//                 horario.hora = horario.hora + 1;
-//             }
-//             let segundosFormatada = horario.segundos < 10 ? `0${horario.segundos}` : horario.segundos;
-//             let minutosFormatada = horario.minutos < 10 ? `0${horario.minutos}` : horario.minutos;
-//             let horaFormatada = horario.horas < 10 ? `0${horario.horas}` : horario.horas;
-//             timer.innerHTML =  `${horaFormatada}:${minutosFormatada}:${segundosFormatada}`;
-//             console.log(`${horaFormatada}:${minutosFormatada}:${segundosFormatada}`);
-//     }
-
-
-//     pausar.addEventListener("click", () => {
-//         clearInterval(intervalID);
-//     });
-
-//     zerar.addEventListener("click", () => {
-//         clearInterval(intervalID);
-//         horario.horas = 0;
-//         horario.minutos = 0;
-//         horario.segundos = 0;
-//         let segundosFormatada = horario.segundos < 10 ? `0${horario.segundos}` : horario.segundos;
-//         let minutosFormatada = horario.minutos < 10 ? `0${horario.minutos}` : horario.minutos;
-//         let horaFormatada = horario.horas < 10 ? `0${horario.horas}` : horario.horas;
-//         timer.innerHTML =  `${horaFormatada}:${minutosFormatada}:${segundosFormatada}`;
-
-//     })
-    
-// })();
-
-
 (function(){
     const timer = document.querySelector(".timer");
     const iniciar = document.querySelector(".iniciar");
     const pausar = document.querySelector(".pausar");
     const zerar = document.querySelector(".zerar");
 
-    let hora = 23;
-    let minuto = 59;
-    let segundo = 50;
+    let hora = 0;
+    let minuto = 0;
+    let segundo = 0;
     let intervalID = null;
         
     formatarHorario();
